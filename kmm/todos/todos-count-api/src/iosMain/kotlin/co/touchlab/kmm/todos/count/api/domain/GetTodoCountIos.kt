d0109138d65295f97832663fc7b88f65b189e31a
AKJAW <akjaworski1@gmail.com>
2022-02-03 14:56:06 +0100

    change wrapper to use interface delegation

--- kmm/todos/todos-count-api/src/iosMain/kotlin/co/touchlab/kmm/todos/count/api/domain/GetTodoCountIos.kt
+++ kmm/todos/todos-count-api/src/iosMain/kotlin/co/touchlab/kmm/todos/count/api/domain/GetTodoCountIos.kt
@@ -1,11 +1,5 @@
 package co.touchlab.kmm.todos.count.api.domain
 
-import co.touchlab.kmm.todos.count.api.data.TodoCount
-import kotlinx.coroutines.flow.Flow
-
 class GetTodoCountIos(
     private val getTodoCount: GetTodoCount
-){
-
-    fun invoke(): Flow<TodoCount> = getTodoCount()
-}
+) : GetTodoCount by getTodoCount
